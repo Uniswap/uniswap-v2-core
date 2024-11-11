4777b36c6aaabe0b7edbf191f5ba0bed748039a8
Skye <yeeqianskye@gmail.com>
2024-11-11 22:50:41 +0800

    fix bug

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -7,10 +7,10 @@ contract UniswapV2Factory is IUniswapV2Factory {
     address public feeTo;
     address public feeToSetter;
 
-    mapping(address => mapping(address => address)) public getPair;
+    mapping(address => mapping(address => mapping(uint256 => address))) public getPair; // getPair[token0][token1][feerate] || getPair[token1][token0][feerate]
     address[] public allPairs;
 
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint feeRate, uint);
 
     constructor(address _feeToSetter) public {
         feeToSetter = _feeToSetter;
@@ -20,28 +20,23 @@ contract UniswapV2Factory is IUniswapV2Factory {
         return allPairs.length;
     }
 
-    function createPair(address tokenA, address tokenB, uint256 feerate) external returns (address pair) {
+    function createPair(address tokenA, address tokenB, uint256 feeRate) external returns (address pair) {
         require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(feerate >= 0 && feerate <= 1e18, 'UniswapV2: FEE_RATE_OVERFLOW');
+        require(feeRate >= 0 && feeRate <= 1e18, 'UniswapV2: FEE_RATE_OVERFLOW');
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+        require(getPair[token0][token1][feeRate] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
         bytes memory bytecode = type(UniswapV2Pair).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1, feeRate));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
         IUniswapV2Pair(pair).initialize(token0, token1);
-        IUniswapV2Pair(pair).setFeeRate(feerate);
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        IUniswapV2Pair(pair).setFeeRate(feeRate);
+        getPair[token0][token1][feeRate] = pair;
+        getPair[token1][token0][feeRate] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
-        emit PairCreated(token0, token1, pair, allPairs.length);
-    }
-
-    function setFeeRate(address pool, uint256 feerate) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
-        IUniswapV2Pair(pool).setFeeRate(feerate);
+        emit PairCreated(token0, token1, pair, feeRate, allPairs.length);
     }
 
     function setLpMtRatio(address pool, uint ratio) external {
--- contracts/interfaces/IUniswapV2Factory.sol
+++ contracts/interfaces/IUniswapV2Factory.sol
@@ -1,17 +1,19 @@
 pragma solidity >=0.5.0;
 
 interface IUniswapV2Factory {
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint feeRate);
 
     function feeTo() external view returns (address);
     function feeToSetter() external view returns (address);
 
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function getPair(address tokenA, address tokenB, uint256 feeRate) external view returns (address pair);
     function allPairs(uint) external view returns (address pair);
     function allPairsLength() external view returns (uint);
 
-    function createPair(address tokenA, address tokenB) external returns (address pair);
+    function createPair(address tokenA, address tokenB, uint256 feerate) external returns (address pair);
 
     function setFeeTo(address) external;
     function setFeeToSetter(address) external;
+
+    function setLpMtRatio(address pool, uint ratio) external;
 }
